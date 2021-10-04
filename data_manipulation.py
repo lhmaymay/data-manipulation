4ab1ed3b5162accca890d87a41ea6adfd1998664
lhmaymay <36777339+lhmaymay@users.noreply.github.com>
2021-10-04 13:54:07 -0700

    Update data_manipulation.py

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,1 +1,21 @@
 
+import pandas as pd
+churn = pd.read_csv("/content/churn.csv")
+
+# compare to sql
+# DESC CHURN;
+churn.dtypes
+# SELECT * FROM CHURN LIMIT 5;
+churn.head(5)
+# SELECT CustomerId, Geography FROM CHURN;
+churn[['CustomerId', 'Geography']]
+churn[churn.Geography == 'France'][['CustomerId','Geography']][:5]
+
+churn.Geography.unique()
+# array(['France', 'Spain', 'Germany'], dtype=object)
+churn.Geography.nunique()
+
+churn.Geography.value_counts()
+churn[['Geography','Age']].groupby('Geography').mean().sort_values(by='Age', ascending=False)
+
+
